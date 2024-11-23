50fa72eb960d37daea86d7a4837ce378ba7b355a
Victor Nguyen <victornguyen0702@gmail.com>
2024-11-23 16:13:15 +0000

    test: add test_get_product_link test

--- scrapers/tests/scrapers/iga_test.py
+++ scrapers/tests/scrapers/iga_test.py
@@ -1,9 +1,11 @@
 import pytest
 
-from scrapers.iga import get_iga_product, get_product_id
+from scrapers.iga import get_iga_product, get_product_link
 
-def test_get_product_id():
+def test_get_product_link():
     """Convert product url to id"""
-    assert get_product_id("https://www.igashop.com.au/product/a2-milk-full-cream-milk-726347") == 726347
+    assert get_product_link("A2 Milk Full Cream Milk") == "a2-milk-full-cream-milk"
+
+
 
 
