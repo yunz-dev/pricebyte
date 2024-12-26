0478350ce910000be08fdc66f23f7ab9238b9e17
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-26 10:40:27 +0000

    feat: add footer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -20,6 +20,7 @@ export default function Header() {
           <Search className="absolute left-3 text-gray-400" size={18} />
           <Input type="text" placeholder="Search products" className="pl-10 pr-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-300"/>
         </div>
+        {/* ADD PAGE LINKS FOR ICONS*/}
         <User className="text-gray-500 cursor-pointer hover:text-black" size={20} />
         <ShoppingCart className="text-gray-500 cursor-pointer hover:text-black" size={20} />
       </div>
