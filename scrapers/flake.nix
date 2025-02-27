05045566c413a56e764afac827da34e124ebf2a3
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-28 10:58:24 +1100

    docs: addead readme to backend

---
+++ scrapers/flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems, nixpkgs, ... }@inputs:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3
+            pkgs.python312Packages.pip
+            pkgs.tailwindcss
+          ];
+
+          shellHook = '''';
+        };
+      });
+    };
+}
