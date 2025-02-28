47b4f5fd93aebf5d34227363a24e7ba435e2fa62
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-28 11:46:58 +1100

    testing ghactions

--- backend/src/main/java/com/yunz/pricebyte/PricebyteApplication.java
+++ backend/src/main/java/com/yunz/pricebyte/PricebyteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PricebyteApplication {
 
+  //test
 	public static void main(String[] args) {
 		SpringApplication.run(PricebyteApplication.class, args);
 	}
