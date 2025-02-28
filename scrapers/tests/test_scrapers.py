686c7b08175216aab7628510e11f059d2ecf3b45
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-01 02:09:13 +1100

    test: add coles test for coke

--- scrapers/tests/test_scrapers.py
+++ scrapers/tests/test_scrapers.py
@@ -31,3 +31,19 @@ def test_scrape_coles_milk(coles_scraper):
         assert result["original_price"] > result["price"]
     else:
         assert result["original_price"] is None
+
+def test_scrape_coles_coke(coles_scraper):
+    """
+    Test that the scraper correctly extracts coke name, price and discount.
+    """
+    result = coles_scraper.scrape_coles_product("https://www.coles.com.au/product/coca-cola-classic-soft-drink-bottle-1.25l-123011")
+
+    assert isinstance(result, dict)
+    assert "name" in result and result["name"] == "Coca-Cola Classic Soft Drink Bottle | 1.25L"
+    assert "price" in result and result["price"]
+
+    if result["discounted"]:
+        assert "original_price" in result and result["original_price"]
+        assert result["original_price"] > result["price"]
+    else:
+        assert result["original_price"] is None
