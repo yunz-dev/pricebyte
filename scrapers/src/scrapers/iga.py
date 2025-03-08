450fabd377a313c2f51e109a5c426f18a273b3f9
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-03-08 19:13:06 +1100

    fix: styling + value/error bugs

--- scrapers/src/scrapers/iga.py
+++ scrapers/src/scrapers/iga.py
@@ -1,6 +1,7 @@
+import re
+
 import requests
 from utils.model import Product
-import re
 
 
 def fetch_product(url: str, store: int, product: int) -> dict:
@@ -12,13 +13,17 @@ def fetch_product(url: str, store: int, product: int) -> dict:
         print(f"Error fetching data: {e}")
         return {}
 
+
 def get_product_link(product_name: str) -> str:
     product_name = product_name.replace("-", " ")
     product_name = re.sub(r"[^\w\s]", "", product_name).split()
     return "-".join(product_name).lower()
 
+
 def get_iga_product(product_id: int, store_id: int = 32600) -> Product:
-    data = fetch_product("https://www.igashop.com.au/api/storefront", store_id, product_id)
+    data = fetch_product(
+        "https://www.igashop.com.au/api/storefront", store_id, product_id
+    )
 
     product_name = data.get("name")
     product_link = get_product_link(product_name)
@@ -40,14 +45,24 @@ def get_iga_product(product_id: int, store_id: int = 32600) -> Product:
             weight_type = "g"
 
     # replace all letters and symbols in price data
+
     try:
-        price = float(re.sub(r'[^0-9.]', '', data.get("price")))
-        unit_price = float(re.sub(r'[^0-9.]', '', data.get("unitPrice")))
-        original_price = float(re.sub(r'[^0-9.]', '', data.get("wasPrice"))) if data.get("wasPrice") else -1
+        price = float(re.sub(r"[^0-9.]", "", data.get("price", "")))
+        try:
+            unit_price = float(re.sub(r"[^0-9.]", "", data.get("unitPrice", "")))
+        except ValueError:
+            unit_price = price
+
+        try:
+            original_price = (
+                float(re.sub(r"[^0-9.]", "", data.get("wasPrice", "")))
+                if data.get("wasPrice")
+                else price
+            )
+        except ValueError:
+            original_price = price
     except ValueError:
-        price = -1
-        unit_price = -1
-        original_price = -1
+        price = unit_price = original_price = -1
 
     return Product(
         store="IGA Store",
@@ -61,6 +76,8 @@ def get_iga_product(product_id: int, store_id: int = 32600) -> Product:
         image_url=data.get("primaryImage", {}).get("default"),
         product_url=product_url,
         weight=f"{weight}{weight_type}",
-        description=data.get("description").replace("<br/>", "").replace("<br />", "").replace("|", " |"),
+        description=data.get("description")
+        .replace("<br/>", "")
+        .replace("<br />", "")
+        .replace("|", " |"),
     )
-
