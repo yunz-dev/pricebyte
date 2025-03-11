75a25f310da5b966baf89f2e0918c5df02e19735
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-12 03:12:39 +1100

    feat: add footer to page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import Header from "./components/ui/Header"
+import Footer from "./components/ui/Footer"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="flex flex-col items-center justify-center w-full min-h-svh">
         <Button>Look ShadCN is installed</Button>
       </div>
+      <Footer />
     </div>
   )
 }
