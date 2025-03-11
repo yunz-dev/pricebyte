a92ce8b26be04131b3ba24371c34cbed4857a4ca
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-12 03:11:38 +1100

    feat: add a header for the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button"
+import Header from "./components/ui/Header"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Look ShadCN is installed</Button>
+    <div className="w-full">
+      <Header />
+      <div className="flex flex-col items-center justify-center w-full min-h-svh">
+        <Button>Look ShadCN is installed</Button>
+      </div>
     </div>
   )
 }
