cb74d735d81f772f3230396bc1f4895cd93bc2d7
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-12 19:03:46 +1100

    fix: delete index.css contents and add header/footer to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button"
+import Header from "./components/header"
+import Footer from "./components/footer"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Look ShadCN is installed</Button>
+    <div className="w-full">
+      <Header/>
+      <div className="flex flex-col items-center justify-center w-full min-h-svh">
+        <Button>Look ShadCN is installed</Button>
+      </div>
+      <Footer />
     </div>
   )
 }
