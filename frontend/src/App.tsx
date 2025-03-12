80ff057cf951a16ff8bc1a74a0cbf6d506ee4375
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-12 23:56:20 +1100

    feat: added basic specification component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button"
 import Header from "./components/header"
 import Footer from "./components/footer"
-
+import ProductSpecs from "./components/productSpecs"
 function App() {
   return (
     <div className="w-full">
       <Header/>
+      <ProductSpecs />
       <div className="flex flex-col items-center justify-center w-full min-h-svh">
         <Button>Look ShadCN is installed</Button>
       </div>
