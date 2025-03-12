ee3e6b56284552ec08a21f5abe295597481a34ba
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-12 18:57:47 +1100

    feat: add footer

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+    return (
+      <footer className="text-gray-500 text-sm text-center py-4">
+          Made by <a href="https://github.com/yunz-dev/pricebyte" className="hover:underline">Yunz and Co. </a>
+          Licensed under <a href="https://opensource.org/license/gpl-3.0" className="hover:underline">GPL-3.0.</a>
+      </footer>
+    );
+}
