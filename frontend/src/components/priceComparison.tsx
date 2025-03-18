d114a7ca98fbc0c66a2303acd071ba3560b38c40
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-19 02:31:35 +1100

    feat: add store logos to price comparison

--- frontend/src/components/priceComparison.tsx
+++ frontend/src/components/priceComparison.tsx
@@ -4,10 +4,10 @@ import { Card } from "./ui/card";
 export default function PriceComparison() {
 
   const [data, setData] = useState([
-    { store: "Woolworths", storeLogo: "/vite.svg", price: 0.00, lastUpdated: "2025-01-29T15:30:00.000Z" },
-    { store: "IGA", storeLogo: "/vite.svg", price: 1.00, lastUpdated: "2025-02-25T15:30:00.000Z" },
-    { store: "Coles", storeLogo: "/vite.svg", price: 2.00, lastUpdated: "2025-01-12T15:30:00.000Z" },
-    { store: "Aldi", storeLogo: "/vite.svg", price: 3.00, lastUpdated: "2025-03-16T15:30:00.000Z" }
+    { store: "Woolworths", storeLogo: "/store-logos/woolworths.svg", price: 0.00, lastUpdated: "2025-01-29T15:30:00.000Z" },
+    { store: "IGA", storeLogo: "/store-logos/iga.svg", price: 1.00, lastUpdated: "2025-02-25T15:30:00.000Z" },
+    { store: "Coles", storeLogo: "/store-logos/coles.svg", price: 2.00, lastUpdated: "2025-01-12T15:30:00.000Z" },
+    { store: "Aldi", storeLogo: "/store-logos/aldi.svg", price: 3.00, lastUpdated: "2025-03-16T15:30:00.000Z" }
   ]);
   
   useEffect(() => {
@@ -24,10 +24,10 @@ export default function PriceComparison() {
       <p className="text-lg font-bold">Price Comparison</p>
       <div className="flex flex-col w-full gap-y-5">
         {data.map((item, index) => (
-          <Card key={index} className="flex flex-row justify-between items-center w-full bg-white px-10">  
-            <div className="flex flex-row gap-5">
-              <img src={item.storeLogo}></img> {/* Store Icon */}
-              <div className="flex flex-col gap-1"> {/* Store Name and Last Updated */}
+          <Card key={index} className="flex flex-row justify-between items-center w-full bg-white px-10 min-h-[150px]">  
+            <div className="flex flex-row gap-5 justify-center">
+              <img src={item.storeLogo} className="w-[80px]"></img> {/* Store Icon */}
+              <div className="flex flex-col gap-1  justify-center"> {/* Store Name and Last Updated */}
                 <p className="font-bold">{item.store}</p>
                 <p>Last Updated: {new Date(item.lastUpdated).toLocaleDateString("en-GB")}</p> {/* Product Price */}
               </div>
